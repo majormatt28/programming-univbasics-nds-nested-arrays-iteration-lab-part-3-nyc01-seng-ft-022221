cfe9290a061288a3e8fe0606875335be4e6ca191
Mathew E Newton <elijah2895@hotmail.com>
2021-02-12 01:56:34 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,15 +2,16 @@ def join_nested_strings(src)
   phrase = []
   counter = 0
   while counter < src.count do
+
     element_string = 0
     while element_string < src[counter].length do
-    if src[counter][element_string].is_a?(String) == true
-      phrase << src[counter][element_string]
+      if src[counter][element_string].is_a?(String) == true
+        phrase << src[counter][element_string]
+      end
+      element_string += 1
     end
-    element_string += 1
-  end
     counter += 1
-end
+  end
   p src.join(" ")
 end
 
